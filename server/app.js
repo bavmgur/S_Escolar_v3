c00fef71f28e41c24b931503dd8940f9dfdb43c4
Juan Manuel <juan.15121001@gmail.com>
2019-05-04 10:04:55 -0500

    Modificando archivo index

--- server/app.js
+++ server/app.js
@@ -1,23 +1,13 @@
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-const sassMiddleware = require('node-sass-middleware')
 
 const routes = require('./routes/index')
-
-const srcPath = __dirname + '/sass'
-const destPath = __dirname + '/public'
 
 module.exports = (app) => {
 
     app.use(morgan('dev'))
     app.use(bodyParser.urlencoded({ extended: false }))
     app.use(bodyParser.json())
-    app.use(sassMiddleware({
-        src: srcPath,
-        dest: destPath,
-        debug: true,
-        prefix: '/prefix'
-    }))
 
     routes(app)
 }
--- server/index.js
+++ server/index.js
@@ -1,23 +1,58 @@
 const express = require('express')
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
 
 const conn = require('./models/index').sequelize
-const config = require('./config/config.dev')
 
 
 const app = express()
 
-require('./vue')()
 require('./app')(app)
 
 
-// app.listen(config.PORT, () => console.log(`Aplicacion funcionando en el puerto ${config.PORT}`))
 
-conn
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-        app.listen(config.PORT, () => console.log(`Aplicacion funcionando en el puerto ${config.PORT}`))
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    })
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
+
+async function start() {
+    // Init Nuxt.js
+    const nuxt = new Nuxt(config)
+
+    const { host, port } = nuxt.options.server
+
+    // Build only in dev mode
+    if (config.dev) {
+        const builder = new Builder(nuxt)
+        await builder.build()
+    } else {
+        await nuxt.ready()
+    }
+
+    // Give nuxt middleware to express
+    app.use(nuxt.render)
+
+    // Listen the server
+    conn
+        .authenticate()
+        .then(async() => {
+            consola.ready({
+                message: `Conectado a la base de datos con exito`,
+                badge: true
+            })
+            await app.listen(port, host)
+            consola.ready({
+                message: `Server listening on http://${host}:${port}`,
+                badge: true
+            })
+        })
+        .catch(err => {
+            consola.error({
+                message: `Unable to connect to the database: ${err}`,
+                badge: true
+            })
+        })
+
+}
+
+start()
