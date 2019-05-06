ba7798a6e01c906df5a38510b20f1a14ba0d0e5c
unknown <beiker2203@gmail.com>
2019-05-06 01:13:49 -0500

    Realizando el modal para llamar asistencia

--- pages/auxiliar/asistencia.vue
+++ pages/auxiliar/asistencia.vue
@@ -9,7 +9,8 @@
      <AuxiliarSearchAssistance />
     </div>
     <div class="auxiliar-assistance__assistance pt-4">
-      <h4 class="text-center mb-5 text-uppercase">Lista de Alumnos</h4>
+      <h4 class="text-center mb-4 text-uppercase">Lista de Alumnos</h4>
+      <div class="text-center mb-4"><button class="button" @click="showOrHideModalCallAssistance">LLAMAR ASISTENCIA</button></div>
       <div class="text-center">
         <div v-if="listStudents.listStudents === 0" class="spinner-border text-primary" role="status">
           <span class="sr-only text-center">Loading...</span>
@@ -17,6 +18,10 @@
         <AuxiliarTableAssistance v-else :listStudents="listStudents" />
       </div>
     </div>
+    <ModalAuxiliarCallAssistance 
+      :isOpenModalCallAssistance="isOpenModalCallAssistance" 
+      @hideModal="hideModalCallAssistance"
+    />
   </div>
 </template>
 
@@ -24,17 +29,32 @@
 import AuxiliarFormAsistance from '../../components/auxliar/AuxliarFormAsistance'
 import AuxiliarSearchAssistance from '../../components/auxliar/AuxliarSearchAssistance'
 import AuxiliarTableAssistance from '../../components/auxliar/AuxliarTableAssistance'
+import ModalAuxiliarCallAssistance from '../../components//modals/AuxiliarModalCallAssistance'
 
 export default {
   name: 'AuxliarAssistancePage',
   components: {
     AuxiliarFormAsistance,
     AuxiliarSearchAssistance,
-    AuxiliarTableAssistance
+    AuxiliarTableAssistance,
+    ModalAuxiliarCallAssistance
+  },
+  data() {
+    return {
+      isOpenModalCallAssistance: false
+    }
   },
   computed: {
     listStudents() {
       return this.$store.state.auxiliar.listStudents
+    }
+  },
+  methods: {
+    showOrHideModalCallAssistance() {
+      this.isOpenModalCallAssistance = !this.isOpenModalCallAssistance
+    },
+    hideModalCallAssistance() {
+      this.showOrHideModalCallAssistance()
     }
   },
   async fetch({ store, $axios }) {
@@ -49,6 +69,7 @@ $color-white: #fff;
 $color-back-1: #f6f7fb;
 $color-back-2: #eee;
 $color-black-1: #000;
+$color-primary-1: #007bff;
 
 .auxiliar-assistance {
   background-color: $color-white;
@@ -71,5 +92,18 @@ $color-black-1: #000;
     padding: 2rem 1rem;
     border-bottom: 0.1rem solid $color-back-2;
   }
+}
+
+.button {
+  background-color: $color-primary-1;
+  color: $color-white;
+  padding: 0.8rem 2rem;
+  border-radius: 0.3rem;
+  border: none;
+  font-size: 1.2rem;
+}
+
+h4 {
+  font-size: 2rem;
 }
 </style>
